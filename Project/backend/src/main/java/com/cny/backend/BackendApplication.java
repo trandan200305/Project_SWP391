3e6600f7b337c716464b355b17825e88c8bf7ef0
datvutruno1 <datxayda061005@gmail.com>
2026-06-14 16:39:20 +0700

    test commit

--- Project/backend/src/main/java/com/cny/backend/BackendApplication.java
+++ Project/backend/src/main/java/com/cny/backend/BackendApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
+//test commit
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
