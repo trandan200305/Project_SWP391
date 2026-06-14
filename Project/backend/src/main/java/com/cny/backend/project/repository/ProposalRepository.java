5715cd89cf0422cb9f29e90bdde0d54213154cb0
datvutruno1 <datxayda061005@gmail.com>
2026-06-14 17:42:11 +0700

    create repository for two objects

---
+++ Project/backend/src/main/java/com/cny/backend/project/repository/ProposalRepository.java
@@ -0,0 +1,18 @@
+package com.cny.backend.project.repository;
+
+import com.cny.backend.project.entity.Proposal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProposalRepository extends JpaRepository<Proposal, Integer> {
+    List<Proposal> findByProjectProjectId(Integer projectId);
+
+    List<Proposal> findByFreeLancerFreelancerId(Integer freelanceId);
+
+    Optional<Proposal> findByProjectProjectIdAndFreelancerFreelancerId(Integer projectId, Integer freelancerId);
+
+}
