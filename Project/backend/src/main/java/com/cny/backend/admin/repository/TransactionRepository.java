f6cdcdb5115e3a9c3088db72b6bab8b431c664ac
tiramisumatchalatte <tiramisu6489@gmail.com>
2026-06-18 06:32:03 +0700

    feat(vnpay): create payment and standard transaction entities and repositories

---
+++ Project/backend/src/main/java/com/cny/backend/admin/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.cny.backend.admin.repository;
+
+import com.cny.backend.admin.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+}
