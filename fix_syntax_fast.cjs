d8236e3707a00bd7e0bec0e3b84c093a989c1220
emanchomchuwa <tdan9704@gmail.com>
2026-07-27 01:56:46 +0700

    chore: remove temporary and redundant files

--- fix_syntax_fast.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = 'c:/Users/admin/Downloads/Project_SWP391/Project_SWP391/Project/frontend/src/features/admin/pages/AdminDashboardPage.jsx';
-let content = fs.readFileSync(path, 'utf8');
-content = content.replace(/\{dashboardSubTab === '([^']+)' && \(/g, '{dashboardSubTab === \ && (<>');
-content = content.replace(/\)\}/g, '</>)}');
-fs.writeFileSync(path, content);
-console.log('Fixed syntax!');
